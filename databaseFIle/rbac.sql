f8cd7b0226f6847e66d63ceba1fd061a9632e4ff
chenxy@corp.shengxun.com <chenxy@corp.shengxun.com>
2021-12-31 13:41:07 +0800

    perfect RBACsql file

--- databaseFIle/rbac.sql
+++ databaseFIle/rbac.sql
@@ -9,26 +9,33 @@ CREATE TABLE AdminUser (
 ) COMMENT='用户表';
 
 CREATE TABLE AdminUserInfo (
-    userid varchar(50) NOT NULL COMMENT '用户id 主键',
+    userId varchar(50) NOT NULL COMMENT '用户id 主键',
     name varchar(50) COMMENT '昵称',
     sex varchar(4) COMMENT '性别',
     phone TINYINT(32) COMMENT '联系方式',
     address varchar(255) COMMENT '住址',
     introduction varchar(255) COMMENT '个人简介',
     registerTime timestamp(6) COMMENT '注册时间',
-    role varchar(50) COMMENT '角色',
-    PRIMARY KEY (userid)
+    roleId varchar(50) COMMENT '角色id',
+    PRIMARY KEY (userId)
 ) COMMENT='用户信息表';
 
 CREATE TABLE AdminUserRole (
     id varchar(32) NOT NULL,
-    PRIMARY KEY (id),
+    roleName varchar(50) COMMENT '角色名字',
+    routerId varchar(50) COMMENT '路由id',
+    PRIMARY KEY (id)
 ) COMMENT='用户角色表';
 
 CREATE TABLE AdminUserRouter (
-    userid varchar(50) NOT NULL COMMENT '用户id 主键',
+    id varchar(50) NOT NULL COMMENT '路由id 主键',
+    name varchar(50) COMMENT '路由名字',
+    routerFnid varchar(50) COMMENT '功能id',
+    PRIMARY KEY (id)
 ) COMMENT='用户路由表';
 
-CREATE TABLE AdminUserFunction (
-    userid varchar(50) NOT NULL COMMENT '用户id 主键',
-) COMMENT='用户路由下功能权限表';
+CREATE TABLE AdminUserRouterFunction (
+    id varchar(50) NOT NULL COMMENT '用户id 主键',
+    name varchar(50) COMMENT '功能名字',
+    PRIMARY KEY (id)
+) COMMENT='用户路由下功能表';
